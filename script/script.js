b3f1f6d30bf0fd49e4eb34eb6851a966c76f53cf
Julie Henry <julie@juliehenrydesignllc.com>
2015-12-11 14:51:39 -0500

    trying to add an additional lightbox

--- script/script.js
+++ script/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 	
-	$(".lightbox-link").on("click",function(){
+	$(".lightbox-link").on("click",function(e){ /*(e overrides in callback a default behavior)*/
+		e.preventDefault();
+		
+		var$image = $("chosen-image")
+		
+		$(image)("src", $(this).attr("src"));
+
 		$(".outer-lightbox").show();
+		$("#chosen").attr("src", "monkey_teeth.jpg");
 		$(".outer-lightbox").on("click",function(){
 		$(".outer-lightbox").hide();
 
@@ -11,3 +18,9 @@ $(document).ready(function() {
 
 
 });
+
+
+
+
+
+
